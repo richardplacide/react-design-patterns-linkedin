25c3371bbafe76a661cd793d7bd98ae826c21e8f
Richard Placide <richard.devel@outlook.com>
2023-02-17 23:52:36 -0400

    SplitScreen v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import SplitScreen from "./SplitScreen";
+
+const LeftHandComponent = () => {
+  return (
+    <h1 style={{backgroundColor:'azure'}}>LeftHandComponent</h1>
+  )
+}
+
+const RightHandComponent = () => {
+  return (
+    <p style={{backgroundColor:'lime'}}>RightHandComponent</p>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitScreen 
+    left={LeftHandComponent}
+    right={RightHandComponent}
+    />    
   );
 }
 
