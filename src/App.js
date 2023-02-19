3707f6125459e6897f75050147e7948711edb9dd
Richard Placide <richard.devel@outlook.com>
2023-02-19 14:44:03 -0400

    Start with empty React App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import SplitScreen from './SplitScreen';
-
-const LeftHandComponent = ({name}) => {
-  return (
-    <div style={{backgroundColor:'azure'}}>
-      <h1>LeftHandComponent</h1>
-      <h2>Hello there {name}</h2>
-    </div>
-  )
-}
-
-const RightHandComponent = ({message}) => {
-  return (
-    <p style={{backgroundColor:'lime'}}>RightHandComponent with message: {message}</p>
-  )
-}
 
 
 function App() {
   return (
-    <SplitScreen leftWeight={2}>
-      <LeftHandComponent name="Richard" />
-      <RightHandComponent message="Reach the sky" />
-    </SplitScreen>
+    <h1>Welcome to React Design Patterns</h1>
   );
 }
 
